fb0b917b1014dd1ee8742fc848728bb8464c7d1b
zhangh <506365426@qq.com>
2020-04-13 11:05:27 +0800

    style:

--- work-problems/07 entry.vendor 抽离（不打包）第三方库，减小构建包的体积/lesson/build/webpack.prod.js
+++ work-problems/07 entry.vendor 抽离（不打包）第三方库，减小构建包的体积/lesson/build/webpack.prod.js
@@ -1,21 +1,22 @@
 // const merge = require("webpack-merge");
 // const commonConfig = require("./webpack.common.js");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 const prodConfig = {
-  mode: "production",
+  mode: 'production',
   // externals: ["lodash","jquery"],
   // devtool: "cheap-module-source-map",
   // development cheap-module-eval-source-map
   // production cheap-module-source-map
   output: {
-    filename: "[name].[contenthash].js",
-    chunkFilename: "[name].[contenthash].chunk.js"
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].chunk.js'
   },
-  externals: ["lodash"],
+  // 将 lodash 抽离不打包到构建文件中
+  externals: ['lodash'],
   module: {
     rules: [
       {
@@ -25,23 +26,23 @@ const prodConfig = {
           // "style-loader",
           // "css-loader",
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 2,
               modules: true
             }
           },
-          "sass-loader",
-          "postcss-loader"
+          'sass-loader',
+          'postcss-loader'
         ]
       },
       {
         test: /\.css$/,
         use: [
-          //'style-loader',
+          // 'style-loader',
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader"
+          'css-loader',
+          'postcss-loader'
         ]
       }
     ]
@@ -49,12 +50,12 @@ const prodConfig = {
   plugins: [
     // 对我们引入的CSS文件进行代码分割，暂时对`HMR support`模块热更新不支持，所以建议是用在生成环境下
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[name].chunk.css"
+      filename: '[name].css',
+      chunkFilename: '[name].chunk.css'
     }),
     new CleanWebpackPlugin({
       verbose: true, // 在命令窗口中打印`clean-webpack-plugin`日志
-      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, "../dist")] // 清除的文件/文件夹
+      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, '../dist')] // 清除的文件/文件夹
     })
   ],
   optimization: {
