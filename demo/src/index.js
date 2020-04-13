d709d965c2b0603d1d68013b47d417e8e21516fc
zhangh <506365426@qq.com>
2020-04-13 19:03:52 +0800

    chore: webpack.config.js 文件配置

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+// import Vue from 'vue'
+// import Vuex from 'vuex'
 import _ from 'lodash'
+import $ from 'jquery';
+// import b from '@/b'
 
 function component () {
   const element = document.createElement('div');
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  console.log(Vue)
-  console.log(Vuex)
-
+  // console.log(Vue)
+  // console.log(Vuex)
+  // console.log(b)
+  console.log('jquery ', $)
   return element;
 }
 
