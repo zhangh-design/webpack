628fb96d80a4975a889e6f465e685177c796646a
zhangh <506365426@qq.com>
2020-04-15 11:10:55 +0800

    docs: devServer配置

--- demo/build/webpack.dev.js
+++ demo/build/webpack.dev.js
@@ -1,11 +1,46 @@
+/* eslint-disable no-unused-vars */
 const path = require('path');
+const config = require('../config')
+const merge = require('webpack-merge')
+const webpack = require('webpack')
+const baseWebpackConfig = require('./webpack.base.js')
 
-// eslint-disable-next-line no-unused-vars
-function resolve (dir) {
-  return path.join(__dirname, '.', dir)
-}
+const HOST = process.env.HOST
+const PORT = process.env.PORT && Number(process.env.PORT)
 
-module.exports = {
-  mode: 'development'
+const devWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'development',
+  devtool: config.dev.devtool,
+  devServer: {
+    host: HOST || config.dev.host,
+    port: PORT || config.dev.port,
+    clientLogLevel: config.dev.clientLogLevel,
+    hot: config.dev.hot,
+    hotOnly: config.dev.hotOnly,
+    compress: config.dev.compress,
+    open: config.dev.open,
+    overlay: config.dev.overlay ? { warnings: false, errors: true } : false,
+    quiet: config.dev.quiet,
+    proxy: config.dev.proxy,
+    publicPath: config.dev.assetsPublicPath,
+    watchOptions: {
+      poll: config.dev.poll
+    },
+    historyApiFallback: {
+      rewrites: [
+        // 404 页面 跳转到 index.html
+        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') }
+      ]
+    }
+  },
+  plugins: [
+    // 导入自定义环境变量
+    new webpack.DefinePlugin({
+      'process.env': require('../config/dev.env')
+    }),
+    // 启用热更新，配合 hot和hotOnly 使用
+    new webpack.HotModuleReplacementPlugin()
+  ]
+})
 
-}
+module.exports = devWebpackConfig
