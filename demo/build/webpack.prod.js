247aef2f3bb8da6ffab9da7c8cd3036c7f16b09f
zhangh <506365426@qq.com>
2020-04-16 11:59:40 +0800

    chore: cacheGroups增加name属性

--- demo/build/webpack.prod.js
+++ demo/build/webpack.prod.js
@@ -37,6 +37,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       name: true,
       cacheGroups: {
         'split-lodash': {
+          name: 'split-lodash', // 打包后 chunk 的名称
           test: module => {
             return /lodash/.test(module.context)
           },
@@ -44,6 +45,7 @@ const webpackConfig = merge(baseWebpackConfig, {
           filename: utils.assetsPath('vendor/split-lodash.js')
         },
         'split-vue': {
+          name: 'split-vue',
           test: module => {
             return /vue|vuex|vue-router/.test(module.context)
           },
@@ -51,6 +53,7 @@ const webpackConfig = merge(baseWebpackConfig, {
           filename: utils.assetsPath('vendor/split-vue.js')
         },
         vendors: {
+          name: 'split-vendors',
           test: /[\\/]node_modules[\\/]/,
           priority: -20,
           filename: utils.assetsPath('vendor/vendors.js')
@@ -81,9 +84,9 @@ const webpackConfig = merge(baseWebpackConfig, {
       // 添加指定的chunk，多页应用时需要动态指定，单页不用配置（不配置就会引入所有页面的资源）
       // 在配置多个页面时，每个页面注入的thunk应该是不相同的，需要通过该配置为不同页面注入不同的thunk
       // 比如：登录页面`chunks: ['login']`，主页面`chunks: ['main']`
-      // chunks: ['main',''],
+      // chunks: ['app'],
       inject: true, // 默认 true，将脚本注入到body元素的底部
-      // 美化 html文件，去除空格、注释等（ production 时使用）
+      // 美化 html 文件，去除空格、注释等（ production 时使用）
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -92,7 +95,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       // 4.2.0 版本已经移除 'dependency'
       // 允许指定的thunk在插入到html文档前进行排序，旧版本（例如^3.2.0）会配置为 'dependency'
       // 多页面中一般会提取公共部分的chunk，这个时候一个html页面会引入多个chunk，而这些chunk之间是有依赖关系的，即必须按照顺序用script标签引入，chunksSortMode是用来指定这种顺序的排序规则，dependency是指按照依赖关系排序。
-      // 配置为 'dependency' 可能会出现 `Cyclic dependency   错误：循环依赖` 的问题，可以改成 auto或none
+      // 旧版配置为 'dependency' 可能会出现 `Cyclic dependency   错误：循环依赖` 的问题，可以升级插件到最新
+      // `Cyclic dependency`网上的解决办法设置为`none`但这样页面加载顺序就不能保证了，可能会出现样式被覆盖的现象
       chunksSortMode: 'auto'
     })
   ]
