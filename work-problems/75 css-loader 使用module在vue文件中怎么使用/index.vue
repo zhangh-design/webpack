b44515314be98e4bbb9e491591b92425f394d402
zhangh <506365426@qq.com>
2020-07-14 10:59:02 +0800

    docs:

---
+++ work-problems/75 css-loader 使用module在vue文件中怎么使用/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="$style.userName">11111 <p :class="$style.textCls">22222</p></div>
+  <!-- <common-user></common-user> -->
+</template>
+
+<script>
+// 用户管理
+export default {}
+</script>
+
+<style scoped lang="scss"></style>
+<style module lang="scss">
+@import "./user.scss";
+.user-name {
+  display: flex;
+  transform: translate(60px);
+}
+</style>
